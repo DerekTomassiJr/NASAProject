c1284771ad22de0d561830f2576856fb3de4fc42
DerekTomassiJr <derektomassi@me.com>
2024-02-29 20:53:06 -0500

    Adding File Structure & Node Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from "./components/Navbar";
+import Query from "./pages/Query";
+import Report from "./pages/Report";
+import Reports from "./pages/Reports";
+import { Route, Routes } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <h1>Test</h1>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={ <Query /> } />
+          <Route path="/query" element={ <Query /> } />
+          <Route path="/report" element={ <Report /> } />
+          <Route path="/reports" element={ <Reports /> } />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import rocketShipLogo from "../images/rocket_ship_logo.png";
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <Link to="/" className="site-title"><img id="site_logo" src={ rocketShipLogo } alt="SiteLogo"/></Link>
+            <ul>
+                <CustomLink to="/query">Query</CustomLink>
+                <CustomLink to="/report">Report</CustomLink>
+                <CustomLink to="/reports">Reports</CustomLink>
+            </ul>
+        </nav>
+    );
+}
+
+function CustomLink({ to, children, ...props } : any) {
+    const resolvedPath = useResolvedPath(to);
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true});
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+    )
+}
